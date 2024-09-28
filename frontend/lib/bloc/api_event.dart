8be556d10b3960216523c973d007bb20f29b8056
Jithin Kunjachan <jithin1986@gmail.com>
2024-09-28 18:19:51 +0200

    clean up

--- frontend/lib/bloc/api_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'api_bloc.dart';
-
-@immutable
-abstract class ApiEvent {}
-
-class SnapRaidSyncEvent extends ApiEvent {
-  final String path;
-  SnapRaidSyncEvent(this.path);
-}
--- frontend/lib/bloc/websocket_event.dart
+++
@@ -1,4 +0,0 @@
-part of 'websocket_bloc.dart';
-
-@immutable
-abstract class WebsocketEvent {}
